21e546384af8e4585912f751c499142ce68e85a2
Satyajit <satyajit@kotaielectronics.com>
2024-02-16 02:50:06 +0530

    port no chnage

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 80;
 
 app.use(bodyParser.json());
 
