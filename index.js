45d96425a83d1736b9aab7fc277fbf1138a181bc
Satyajit <satyajit@kotaielectronics.com>
2024-02-16 00:07:33 +0530

    test commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5001;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "server running...." });
+});
+
+app.get('/srv/hi', (req, res) => {
+    res.send({
+        message: "Hello I am Testing Server, Happy To See You.."
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
